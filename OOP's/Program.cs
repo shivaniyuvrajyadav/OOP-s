b1aec55b413d859acad5330639e55f8f8e34c34d
Admin <Admin@LAPTOP-V3GL5398>
2024-08-06 12:48:27 +0530

    Add project files.

---
+++ OOP's/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace OOP_s
+{
+    public class Program
+    {
+       
+    }
+    private class Science
+    {
+
+    }
+    public static class Social
+    {
+
+    }
+    public abstract class Maths { }
+
+    public class Student
+    {
+        Maths m = new Maths();
+
+        Program p = new Program();
+
+        Science s = new Science();
+
+        Social sc = new Social();
+    }
+}
+
+/*  is it possiblle to prevent object creation of a class in c#?
+ *  
+ *   Object creation of a class can be prevented by
+ *   Abstract Class
+ *   Private Class
+ *   Static Class
+ */
