1d6748c0c363a3f23dff94d0970574fa9b5334df
Admin <Admin@LAPTOP-V3GL5398>
2024-08-06 12:57:26 +0530

    .

---
+++ OOP's/Propertycs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_s
+{
+    internal class Propertycs
+    {
+        private int myproperty;
+
+        public int Myproperty
+        {
+            get { return myproperty; }
+            set { myproperty = value; }
+        }
+
+        public string Myproperty1 { get; set; }
+    }
+}
+
+/*  what is a property?
+ *  a property is a class member that provides a flexible mechanism to read and write private field.
+ * 
+ * 
+ */
